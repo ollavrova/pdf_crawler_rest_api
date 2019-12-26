652e11f32ce03d3e889d2c02572aac8374afdff3
ollavrova <krocozabr@gmail.com>
2019-12-26 07:35:49 +0200

    added tests

--- pdf_crawler/tests.py
+++ pdf_crawler/tests.py
@@ -1,6 +1,16 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.test import TestCase, Client
+from rest_framework.reverse import reverse
 
-from django.test import TestCase
 
-# Create your tests here.
+class TestCase(TestCase):
+
+    client = Client()
+
+    def test_endpoints(self):
+        """
+        test for endpoints
+        """
+        self.assertEqual(self.client.get(reverse('pdf_crawler:home')).status_code, 200)
+        self.assertEqual(self.client.get(reverse('pdf_crawler:documents-list')).status_code, 200)
+        self.assertEqual(self.client.get(reverse('pdf_crawler:documents-detail')).status_code, 200)
+        self.assertEqual(self.client.get(reverse('pdf_crawler:urls-list')).status_code, 200)
